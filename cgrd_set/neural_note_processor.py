54b3f5750b3a32d47a32514e80c4c9442f9e9c0c
Ching-Heng <chingheng113@gmail.com>
2019-09-24 17:25:13 -0400

    bk

--- cgrd_set/neural_note_processor.py
+++ cgrd_set/neural_note_processor.py
@@ -6,37 +6,26 @@ current_path = os.path.dirname(__file__)
 
 
 note_1 = pd.read_csv('14653_腦神經內科報告_1.csv')
-print(note_1.columns.values)
-# selected_cols = ['報告01']
+# print(note_1.columns.values)
+
+selected_cols = ['檢查項目', '報告01']
 note_1 = note_1[selected_cols]
+# remove M29-079: carotid report...
+note_1 = note_1[note_1['檢查項目'] != 'M29-079']
 note_1 = note_1[0:100]
 
+delimiters = ['Conclusion:', 'Comments:', 'Interpretation:', 'INTERPRETATION:']
 nlp = spacy.load('en_core_sci_md', disable=['tagger', 'ner'])
-with open('xnote.txt', 'w', encoding="utf-8") as f:
+with open('nu_note.txt', 'w', encoding="utf-8") as f:
     for inx, row in note_1.iterrows():
-        for i in range(len(selected_cols)):
-            parg = str(row[selected_cols[i]])
-            print(parg)
-#             # remove special characters
-#             parg = re.sub(r'(^;)|(;;;)|(=+>)|(={2})|(-+>)|(={2})|(\*{3})', '', parg)
-#             # fix bullet points
-#             parg = re.sub(r'(\d+[.{1}][\D])', r'. \1', parg)
-#             # fix period
-#             parg = re.sub(r'(\s{1}[.])', '. ', parg)
-#             parg = re.sub(r'(\s{1}[.]\s{1})', '. ', parg)
-#             parg = re.sub(r'([.]+)', '.', parg)
-#             # remove multi-spaces
-#             parg = re.sub(' +', ' ', parg)
-#             # remove Chinese
-#             parg = re.sub(r'[\u4e00-\u9fff]+', '', parg)
-#
-#             sentences = nlp(parg)
-#             for sentence in sentences.sents:
-#                 if len(sentence) > 3:
-#                     # remove bullet point number
-#                     see_sentence = re.sub(r'(^\d+\.)|(^\s+)', '', sentence.text)
-#                     # trim white space at the head and the end
-#                     see_sentence = see_sentence.strip()
-#
-#
-# print('done')
+        parg = str(row[selected_cols[1]])
+        for delimiter in delimiters:
+            print(parg.find(delimiter))
+        print('--')
+        # parg = parg.lower()
+        # remove special characters
+        # parg = re.sub(r'(^;)|(;;;)|(=+>)|(_{2})|(={2})|(-{2})|(={2})|(\*{3})', '', parg)
+        # remove Chinese
+        # parg = re.sub(r'[\u4e00-\u9fff]+', '', parg)
+
+print('done')
