8d252b41c7a7598a65d9148b57ad489947bc6fc8
chingheng113 <chingheng113@gmail.com>
2019-10-02 20:52:38 -0400

    bk

--- downstream_tasks/run_classifier.py
+++ downstream_tasks/run_classifier.py
@@ -1009,7 +1009,7 @@ if __name__ == "__main__":
         task_name='carotid',
         output_dir=os.path.join(current_path, 'output'),
         cache_dir='',
-        max_seq_length=128,
+        max_seq_length=512,
         do_train=True,
         do_eval=False,
         do_test=True,
