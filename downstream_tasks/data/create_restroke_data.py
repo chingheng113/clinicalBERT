f2268accb4eeeb55f974714967cab45a1ba742bb
Ching-Heng <chingheng113@gmail.com>
2019-12-30 17:16:07 -0500

    bk

--- downstream_tasks/data/create_restroke_data.py
+++ downstream_tasks/data/create_restroke_data.py
@@ -1,20 +1,26 @@
 import pandas as pd
+import numpy as np
 import re
 
 
 def clean_text(parg):
-    # remove special characters
-    parg = re.sub(r'(^;)|(;;;)|(=+>)|(={2})|(-+>)|(={2})|(\*{3})', '', parg)
-    # fix bullet points
-    parg = re.sub(r'(\d+[.{1}][\D])', r'. \1', parg)
-    # remove multi-spaces
-    parg = re.sub(r' +', ' ', parg)
-    # print(parg)
-    # remove Chinese
-    parg = re.sub(r'[\u4e00-\u9fff]+', '', parg)
-    # remove date
-    parg = re.sub(r'\d{2,4}[/]\d{1,2}[/]\d{1,2}', '', parg)
+    if parg == parg:
+        # remove special characters
+        parg = re.sub(r'(^;)|(;;;)|(=+>)|(={2})|(-+>)|(={2})|(\*{3})', '', parg)
+        # fix bullet points
+        parg = re.sub(r'(\d+[.{1}][\D])', r'. \1', parg)
+        # remove multi-spaces
+        parg = re.sub(r' +', ' ', parg)
+        # print(parg)
+        # remove Chinese
+        parg = re.sub(r'[\u4e00-\u9fff]+', '', parg)
+        # remove date
+        parg = re.sub(r'\d{2,4}[/]\d{1,2}[/]\d{1,2}', '', parg)
     return parg
 
 
-df = pd.read_csv('recurrent_stroke.csv')
+df = pd.read_csv('recurrent_stroke_ds.csv')
+df['主訴'] = df['主訴'].apply(clean_text)
+df['病史'] = df['病史'].apply(clean_text)
+df['住院治療經過'] = df['住院治療經過'].apply(clean_text)
+print('done')
