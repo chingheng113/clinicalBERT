0537ebb0ea4a33a37d148fd7fc5b072f15af28ef
Ching-Heng <chingheng113@gmail.com>
2020-01-03 11:50:55 -0500

    bk

--- downstream_tasks/output/performance_restoke.py
+++ downstream_tasks/output/performance_restoke.py
@@ -3,7 +3,7 @@ from sklearn.metrics import roc_curve, auc, confusion_matrix, classification_rep
 import os
 import pickle
 
-model_name = 'c_all_0'
+model_name = 'b_all_0'
 print(model_name)
 read_path = os.path.join('restroke', model_name, 'test_prediction.pickle')
 with open(read_path, 'rb') as f:
@@ -16,20 +16,4 @@ with open(read_path, 'rb') as f:
     p_label_b = (all_logits[:, 1] > 0.5).astype(int)
     print(classification_report(all_labels, p_label_b))
     print(confusion_matrix(all_labels, p_label_b))
-    # fpr = dict()
-    # tpr = dict()
-    # roc_auc = dict()
-    # for i in range(17):
-    #     fpr[i], tpr[i], _ = roc_curve(all_labels[:, i], all_logits[:, i])
-    #     roc_auc[i] = auc(fpr[i], tpr[i])
-    #     print(labels[i])
-    #     print(roc_auc[i])
-    #     p_label_b = (all_logits[:, i] > 0.5).astype(int)
-    #     print(classification_report(all_labels[:, i], p_label_b))
-    #     print(confusion_matrix(all_labels[:, i], p_label_b))
-    # # Compute micro-average ROC curve and ROC area
-    # fpr["micro"], tpr["micro"], _ = roc_curve(all_labels.ravel(), all_logits.ravel())
-    # roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-    # print('------')
-    # print(roc_auc["micro"])
     print('done')
