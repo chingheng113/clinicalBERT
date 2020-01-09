7425cfb556d573275b2a1ff5c0d06160ecfca0f1
Ching-Heng <chingheng113@gmail.com>
2020-01-09 15:26:39 -0500

    bk

--- downstream_tasks/output/performance_restoke_all.py
+++ downstream_tasks/output/performance_restoke_all.py
@@ -1,12 +1,16 @@
 import numpy as np
 from sklearn.metrics import roc_curve, auc, confusion_matrix, classification_report
+from sklearn.metrics import precision_recall_fscore_support as score
 import os
 import pickle
 
 aucs = []
+precisions =[]
+recalls =[]
+fscores =[]
 for i in range(10):
-    model_name = 'sb_all_'+str(i)
-    print(model_name)
+    model_name = 'c_all_'+str(i)
+    # print(model_name)
     read_path = os.path.join('restroke', model_name, 'test_prediction.pickle')
     with open(read_path, 'rb') as f:
         data = pickle.load(f)
@@ -17,7 +21,15 @@ for i in range(10):
         aucs.append(roc_auc)
         # print(roc_auc)
         p_label_b = (all_logits[:, 1] > 0.5).astype(int)
-        print(classification_report(all_labels, p_label_b))
-        print(confusion_matrix(all_labels, p_label_b))
-print(np.mean(aucs), np.std(aucs))
+        precision, recall, fscore, support = score(all_labels, p_label_b, average='macro')
+        precisions.append(precision)
+        recalls.append(recall)
+        fscores.append(fscore)
+        # print(classification_report(all_labels, p_label_b))
+        # print(confusion_matrix(all_labels, p_label_b))
+
+print(round(np.mean(precisions),3), round(np.std(precisions),3))
+print(round(np.mean(recalls),3), round(np.std(recalls),3))
+print(round(np.mean(fscores),3), round(np.std(fscores),3))
+print(round(np.mean(aucs),3), round(np.std(aucs),3))
 print('done')
