c915d0b6f341d53991db66d6d4f22834b0639bea
Ching-Heng <chingheng113@gmail.com>
2020-01-13 17:26:01 -0500

    bk

--- downstream_tasks/data/create_carotid2_data.py
+++ downstream_tasks/data/create_carotid2_data.py
@@ -6,6 +6,21 @@ import re
 from sklearn.model_selection import train_test_split
 current_path = os.path.dirname(__file__)
 
+def clean_text(parg):
+    if parg == parg:
+        a = re.search('findings:', parg, re.IGNORECASE)
+        parg = re.sub(r'(:nil)', '', parg)
+        # remove special characters
+        parg = re.sub(r'(\s\.)|(\.{2,})|(:{2,})|(^;)|(;;;)|(=+>)|(={2})|(-+>)|(={2})|(\*{3})', '', parg)
+        # fix bullet points
+        parg = re.sub(r'(\d+[.{1}][\D])', r'. \1', parg)
+        # remove Chinese
+        parg = re.sub(r'[\u4e00-\u9fff]+', ' ', parg)
+        # remove multi-spaces
+        parg = re.sub(r'[\s\t]+', ' ', parg)
+        # remove date
+        parg = re.sub(r'\d{2,4}[/]\d{1,2}[/]\d{1,2}', '', parg)
+    return parg
 
 def save_variable(val, val_name):
     save_path = os.path.join(current_path, val_name)
@@ -14,9 +29,9 @@ def save_variable(val, val_name):
 
 # read data
 data = pd.read_csv('carotid2.csv')
-
+data.rename(columns={'IDCode': 'ID', 'RTRESTXT': 'processed_content'}, inplace=True)
 data.dropna(subset=['processed_content'], axis=0, inplace=True)
-data.rename(columns={'':'ID', '':'processed_content'})
+data['processed_content'] = data['processed_content'].apply(clean_text)
 # id_data = data[['ID']]
 # x_data = data[['processed_content']]
 # y_data = data[['RCCA', 'REICA', 'RIICA', 'RACA', 'RMCA', 'RPCA', 'REVA', 'RIVA', 'BA',
