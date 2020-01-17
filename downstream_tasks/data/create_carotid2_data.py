6babea2f9a779e39f956fd552867aa16dbaf5363
chingheng113 <chingheng113@gmail.com>
2020-01-17 10:46:23 -0500

    bk

--- downstream_tasks/data/create_carotid2_data.py
+++ downstream_tasks/data/create_carotid2_data.py
@@ -22,6 +22,7 @@ def clean_text(parg):
         parg = re.sub(r'\d{2,4}[/]\d{1,2}[/]\d{1,2}', '', parg)
     return parg
 
+
 def save_variable(val, val_name):
     save_path = os.path.join(current_path, val_name)
     with open(save_path, 'wb') as file_pi:
@@ -32,21 +33,9 @@ data = pd.read_csv('carotid2.csv')
 data.rename(columns={'IDCode': 'ID', 'RTRESTXT': 'processed_content'}, inplace=True)
 data.dropna(subset=['processed_content'], axis=0, inplace=True)
 data['processed_content'] = data['processed_content'].apply(clean_text)
-# id_data = data[['ID']]
-# x_data = data[['processed_content']]
-# y_data = data[['RCCA', 'REICA', 'RIICA', 'RACA', 'RMCA', 'RPCA', 'REVA', 'RIVA', 'BA',
-#                'LCCA', 'LEICA', 'LIICA', 'LACA', 'LMCA', 'LPCA', 'LEVA', 'LIVA']]
 
-for i in range(10):
-    training_data, testing_data = train_test_split(data, test_size=0.2, random_state=i)
-    # dir_path = 'round_'+str(i)
-    dir_path = str(os.path.join('carotid', 'round_' + str(i)))
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-    training_data.to_csv(os.path.join(dir_path, 'training_'+ str(i) + '.csv'), index=False)
-    testing_data.to_csv(os.path.join(dir_path, 'testing_' + str(i) + '.csv'), index=False)
-    save_variable(training_data, os.path.join(dir_path, 'training_bert.pickle'))
-    save_variable(testing_data, os.path.join(dir_path, 'test_bert.pickle'))
-    print(i)
+data.to_csv(os.path.join('carotid2', 'testing.csv'), index=False)
+save_variable(data, os.path.join('carotid2', 'test_bert.pickle'))
+
 
 print('done')
