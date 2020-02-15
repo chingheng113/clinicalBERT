b2f1d1fbc88dbd872d990313cd7b2377aaac0f51
Ching-Heng <chingheng113@gmail.com>
2020-02-15 16:29:40 -0500

    bk

---
+++ downstream_tasks/restroke_scripts/m_days/launch.bat
@@ -0,0 +1,3 @@
+swarm -f restroke_bs_all_0.swarm -g 100 -t 6 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_b_all_0.swarm -g 100 -t 6 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_c_all_0.swarm -g 100 -t 6 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
